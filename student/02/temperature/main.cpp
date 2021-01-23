b40369e274b90666e31ba8533499da9dd28ffa3b
Prosper Evergreen <prosperevergreen@gmail.com>
2021-01-23 02:10:26 +0200

    set up conversion to celsius

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -8,14 +8,15 @@ int main()
 
     // Write your code here
 
-    float temperature = 0, fahrenheit = 0;
+    float temperature = 0, fahrenheit = 0, celsius = 0;
 
     cin >> temperature;
 
     fahrenheit = (temperature * 1.8) + 32;
+    celsius = (temperature - 32)/1.8;
 
     cout << temperature << " degrees Celsius is " << fahrenheit <<" degrees Fahrenheit" << endl;
-
+    cout << temperature << " degrees in Fahrenheit is " << celsius <<" degrees Celsius" << endl;
 
     return 0;
 }
