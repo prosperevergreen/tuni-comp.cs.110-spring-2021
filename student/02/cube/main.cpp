5fcdea60680afd76fdd9f2ba2751d0efd4269f00
Prosper Evergreen <prosperevergreen@gmail.com>
2021-01-25 00:53:37 +0200

    Cube task complete

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int max_int = INT_MAX, number = 0, cube = 0;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number < 0) {
+        cout << "Only positive numbers accepted" << endl;
+    } else {
+        cube = number * number * number;
+
+        if (number > 1290) {
+            cout << "Error! The cube of " << number << " is not " << cube << "." << endl;
+        } else {
+            cout << "The cube of " << number << " is " << cube << "." << endl;
+        }
+    }
+
     return 0;
 }
