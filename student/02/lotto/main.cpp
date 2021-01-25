c31c1ff6a64b08d4ff073c8a68aace860efd6f8c
Prosper Evergreen <prosperevergreen@gmail.com>
2021-01-25 04:18:45 +0200

    wk2-lotto: task completed

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -22,9 +22,18 @@ unsigned long int calculate_lottery(int total_balls, int drawn_balls)
 
 int main()
 {
-    int number;
+    int total_num = 0, drawn_num = 0;
     cout << "Enter the total number of lottery balls: ";
-    cin >> number;
-    cout << factorial(number) << endl;
+    cin >> total_num;
+    cout << "Enter the number of drawn balls: ";
+    cin >> drawn_num;
+
+    if (total_num * drawn_num < 0) {
+        cout << "The number of balls must be a positive number." << endl;
+    } else if (drawn_num >= total_num) {
+        cout << "The maximum number of drawn balls is the total amount of balls." << endl;
+    } else {
+        cout << "The probability of guessing all " << drawn_num << " balls correctly is 1/" << calculate_lottery(total_num, drawn_num) << endl;
+    }
     return 0;
 }
