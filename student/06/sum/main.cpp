e2d9d799cabc62b710216b72aa71f7fbe7b0eab9
Prosper Evergreen <prosperevergreen@gmail.com>
2021-03-04 20:32:48 +0200

    Complete 6.1.2: Sum of vector elements

--- student/06/sum/main.cpp
+++ student/06/sum/main.cpp
@@ -31,7 +31,9 @@ int sum_recursive(std::vector<int> v)
 
     // Add your implementation here
     if (v.size() == 1) return v.back();
-    return v.back() + sum_recursive(v.pop_back());
+    const int back = v.back();
+    v.pop_back();
+    return back + sum_recursive(v);
 }
 
 // Do not modify rest of the code, or the automated testing won't work.
