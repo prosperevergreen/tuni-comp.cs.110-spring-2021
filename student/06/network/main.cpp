a23a6b4b05296f52512ba0318fa98da7caf656b4
Prosper Evergreen <prosperevergreen@gmail.com>
2021-03-05 00:43:54 +0200

    Complete 6.4: Network save function implemented

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -1,40 +1,47 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
 
-
-std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false){
+std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false)
+{
     std::vector<std::string> result;
     std::string tmp = s;
 
-    while(tmp.find(delimiter) != std::string::npos)
-    {
+    while (tmp.find(delimiter) != std::string::npos) {
         std::string new_part = tmp.substr(0, tmp.find(delimiter));
-        tmp = tmp.substr(tmp.find(delimiter)+1, tmp.size());
-        if(not (ignore_empty and new_part.empty()))
-        {
+        tmp = tmp.substr(tmp.find(delimiter) + 1, tmp.size());
+        if (not(ignore_empty and new_part.empty())) {
             result.push_back(new_part);
         }
     }
-    if(not (ignore_empty and tmp.empty()))
-    {
+    if (not(ignore_empty and tmp.empty())) {
         result.push_back(tmp);
     }
     return result;
 }
 
+void record_save(std::string id1, std::string id2, std::map<std::string, std::vector<std::string>>& record_book)
+{
+    if (record_book.find(id1) != record_book.end()) {
+        record_book.at(id1).push_back(id2);
+    } else {
+        record_book.insert({ id1, { id2 } });
+    }
+}
 
 
 int main()
 {
     // TODO: Implement the datastructure here
 
+    std::map<std::string, std::vector<std::string>> netword_record;
 
-    while(true){
+    while (true) {
         std::string line;
         std::cout << "> ";
         getline(std::cin, line);
@@ -42,47 +49,53 @@ int main()
 
         std::string command = parts.at(0);
 
-        if(command == "S" or command == "s"){
-            if(parts.size() != 3){
-                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+        if (command == "S" or command == "s") {
+            if (parts.size() != 3) {
+                std::cout << "Erroneous parameters!" << std::endl
+                          << HELP_TEXT;
                 continue;
             }
             std::string id1 = parts.at(1);
             std::string id2 = parts.at(2);
 
             // TODO: Implement the command here!
+            record_save(id1, id2, netword_record);
 
-        } else if(command == "P" or command == "p"){
-            if(parts.size() != 2){
-                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+        } else if (command == "P" or command == "p") {
+            if (parts.size() != 2) {
+                std::cout << "Erroneous parameters!" << std::endl
+                          << HELP_TEXT;
                 continue;
             }
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
 
-        } else if(command == "C" or command == "c"){
-            if(parts.size() != 2){
-                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+        } else if (command == "C" or command == "c") {
+            if (parts.size() != 2) {
+                std::cout << "Erroneous parameters!" << std::endl
+                          << HELP_TEXT;
                 continue;
             }
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
 
-        } else if(command == "D" or command == "d"){
-            if(parts.size() != 2){
-                std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
+        } else if (command == "D" or command == "d") {
+            if (parts.size() != 2) {
+                std::cout << "Erroneous parameters!" << std::endl
+                          << HELP_TEXT;
                 continue;
             }
             std::string id = parts.at(1);
 
             // TODO: Implement the command here!
 
-        } else if(command == "Q" or command == "q"){
-           return EXIT_SUCCESS;
+        } else if (command == "Q" or command == "q") {
+            return EXIT_SUCCESS;
         } else {
-            std::cout << "Erroneous command!" << std::endl << HELP_TEXT;
+            std::cout << "Erroneous command!" << std::endl
+                      << HELP_TEXT;
         }
     }
 }
