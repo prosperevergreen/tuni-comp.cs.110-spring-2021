cbf96b9bacb3e76fc2747c4ed0b07328473f8d9e
Prosper Evergreen <prosperevergreen@gmail.com>
2021-03-04 21:09:59 +0200

    Complete 6.1.3: Vertical print

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -12,9 +12,12 @@ void print_vertical(unsigned int num)
     // ------------
 
     // Add your implementation here
-    if (num < 10) return cout << num << endl;
-    print_vertical(num / 10);
-    cout << num % 10 << endl;
+    if (num < 10) {
+        cout << num << endl;
+    } else {
+        print_vertical(num / 10);
+        cout << num % 10 << endl;
+    }
 }
 
 // Do not modify rest of the code, or the automated testing won't work.
