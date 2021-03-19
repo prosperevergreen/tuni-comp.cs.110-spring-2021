2f083eeaa9a31cc9d5f895b252a2bbc959dcca77
Prosper Evergreen <prosperevergreen@gmail.com>
2021-03-19 02:50:45 +0200

    complete (E) Array operations

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -2,14 +2,14 @@
 
 int greatest_v1(int* itemptr, int size)
 {
-    int* temp_grt = itemptr;
+    int temp_grt = *itemptr;
 
     for (int i = 0; i < size; i++) {
-        if (*(itemptr + i) > *temp_grt)
-            temp_grt = itemptr;
+        if (*(itemptr + i) > temp_grt)
+            temp_grt = *(itemptr + i);
     }
 
-    return *temp_grt;
+    return temp_grt;
 }
 
 int greatest_v2(int* itemptr, int* endptr)
@@ -19,16 +19,17 @@ int greatest_v2(int* itemptr, int* endptr)
 
 void copy(int* itemptr, int* endptr, int* targetptr)
 {
-    for (int i = 0; (itemptr + i) <= endptr; i++) {
+    int size = endptr - itemptr;
+    for (int i = 0; i <= size; i++) {
         *(targetptr + i) = *(itemptr + i);
     }
 }
 
 void reverse(int* leftptr, int* rightptr)
 {
-    int arr_length = rightptr - leftptr;
+    int size = rightptr - leftptr;
 
-    for (int i = 0; i <= arr_length / 2; i++) {
+    for (int i = 0; i <= size / 2; i++) {
         int temp = *(leftptr + i);
         *(leftptr + i) = *(rightptr - i);
         *(rightptr - i) = temp;
